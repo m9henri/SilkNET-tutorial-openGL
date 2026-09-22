309efed67f959567d70751081abc68f0d791ae03
m9henri <missolhenri@gmail.com>
2026-09-23 01:01:47 +0200

    Hello Quad 1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using Silk.NET.Input;
 using Silk.NET.Maths;
 using Silk.NET.Windowing;
+using Silk.NET.OpenGL;
+using System.Drawing;
+
+// https://dotnet.github.io/Silk.NET/docs/opengl/c1/2-hello-quad#element-buffer-objects-ebos
 
 namespace SilkNET_tutorial;
 
 class Program
 {
     private static IWindow _window;
+    private static GL _gl;
+    private static uint _vao;
+    private static uint _vbo;
 
     static void Main(string[] args)
     {
@@ -22,7 +29,7 @@ class Program
         _window.Run();
     }
 
-    private static void OnLoad()
+    private static unsafe void OnLoad()
     {
         _window.Center();
 
@@ -31,11 +38,32 @@ class Program
         {
             input.Keyboards[i].KeyDown += KeyDown;
         }
+
+        _gl = _window.CreateOpenGL();
+
+        _gl.ClearColor(Color.CornflowerBlue);
+        _gl.Clear(ClearBufferMask.ColorBufferBit);
+
+        _vao = _gl.GenVertexArray();
+        _gl.BindVertexArray(_vao);
+
+        float[] vertices =
+        {
+             0.5f,  0.5f,  0.0f,
+             0.5f, -0.5f,  0.0f,
+            -0.5f, -0.5f,  0.0f,
+            -0.5f,  0.5f,  0.0f
+        };
+
+        _vbo = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
+
+        fixed (float* buf = vertices) _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(vertices.Length * sizeof(float)), buf, BufferUsageARB.StaticDraw);
     }
 
     private static void OnUpdate(double deltaTime) { }
 
-    private static void OnRender(double deltaTime) { }
+    private static unsafe void OnRender(double deltaTime) { }
 
     private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
     {
